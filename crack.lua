5035591ed7847c75d64240cdafa804aeee4301ed
Dylan <138546622+x0x37x@users.noreply.github.com>
2024-10-18 19:24:28 +1300

    Update crack.lua

--- crack.lua
+++ crack.lua
@@ -20,7 +20,7 @@ local oldHttp; oldHttp = hookfunction(game.HttpGet, function(self, url)
 		return "v8Ya2UswRWfwQPSyvAj58VyOSmVF9mzApoR516PN91JkgJYBoSyHUaCpRPUA1rs5v9t1Ke16Elr8jgyvAj58VyE3kPVrKPd7u5rcAqARcHOTbD6UZIhATW1o7dov3IOm15cZVvVYsuCJLp8Upw6Vll9nuxapJ9y61VsNBXoXnrGt8e4MqcN26eu6P5TXTjRFlcnkxTQvH1OsRmTggCyt8sr"
   	end
 	if url:match("newintestbeta") then
-		-- 🏳️‍🌈 RETURN UI BACKUP 🏳️‍🌈 --
+		-- 🏳️‍🌈 REDIRECT TO UI BACKUP 🏳️‍🌈 --
 		url = "https://raw.githubusercontent.com/x0x37x/darkx-crack/refs/heads/main/ui.lua"
 	end
   	return oldHttp(self, url)
